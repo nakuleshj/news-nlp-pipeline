5bb89671fc924758ca5d6df56b1caa6b20957d4e
Nakulesh <nakuleshj1998@gmail.com>
2025-07-13 10:33:07 -0400

    Resolve any issues related to IAM roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,14 +37,23 @@ resource "aws_s3_bucket_lifecycle_configuration" "raw_data_bucket_lc" {
     status = "Enabled"
   }
 }
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.enrich_news.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_data_bucket.arn
+}
 
 resource "aws_s3_bucket_notification" "raw_news_writes" {
   bucket = aws_s3_bucket.raw_data_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.enrich_news
+    lambda_function_arn = aws_lambda_function.enrich_news.arn
     events = ["s3:ObjectCreated:*"]
   }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
 }
 
 # Enriched S3 bucket
@@ -86,6 +95,10 @@ data "aws_iam_policy_document" "assume_role" {
 
     actions = ["sts:AssumeRole"]
   }
+}
+
+
+data "aws_iam_policy_document" "lambda_execution_policy" {
   statement {
     sid    = "VisualEditor0"
     effect = "Allow"
@@ -96,6 +109,7 @@ data "aws_iam_policy_document" "assume_role" {
       "arn:aws:secretsmanager:us-east-2:860131551212:secret:news_api-*"
     ]
   }
+
   statement {
     effect = "Allow"
     actions = [
@@ -103,34 +117,42 @@ data "aws_iam_policy_document" "assume_role" {
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-    resources = [
-      "*"
-    ]
+    resources = ["*"]
   }
+
   statement {
     effect = "Allow"
     actions = [
       "s3:*",
       "s3-object-lambda:*"
     ]
-    resources = [
-      "*"
-    ]
+    resources = ["*"]
   }
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  name               = "lambda-execution-role"
+
+resource "aws_iam_role" "ud_lambda_execution_role" {
+  name               = "ud-lambda-execution-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "inline_policy" {
+  role = aws_iam_role.ud_lambda_execution_role.id
+  policy = data.aws_iam_policy_document.lambda_execution_policy.json
+  
 }
 
 resource "aws_lambda_function" "ingest_news" {
   filename      = "./scripts/ingest_news.zip"
   function_name = "ingest_news"
-  role          = aws_iam_role.lambda_execution_role.arn
+  role          = aws_iam_role.ud_lambda_execution_role.arn
   handler       = "ingest_news.lambda_handler"
 
   runtime = "python3.13"
+
+  timeout = 30
+
+  memory_size = 256
 
   layers = [aws_lambda_layer_version.requests_layer.arn]
 
@@ -144,15 +166,16 @@ resource "aws_lambda_function" "enrich_news" {
 
   filename      = "./scripts/enrich_raw_data.zip"
   function_name = "enrich_raw_data"
-  role          = aws_iam_role.lambda_execution_role.arn
+  role          = aws_iam_role.ud_lambda_execution_role.arn
   handler       = "enrich_raw_data.lambda_handler"
 
   runtime = "python3.13"
 
+  timeout = 30
+
   layers = [
-    "arn:aws:lambda:us-east-2:336392948345:layer:AWSSDKPandas-Python313:3",
     aws_lambda_layer_version.nltk_layer.arn,
-    aws_lambda_layer_version.pyarrow_layer.arn
+    aws_lambda_layer_version.pandas_layer.arn
   ]
 
   tags = {
@@ -164,7 +187,7 @@ resource "aws_lambda_function" "enrich_news" {
 resource "aws_lambda_layer_version" "requests_layer" {
 
   layer_name = "requests_layer"
-  filename   = "./layers/requests_layer_payload.zip"
+  filename   = "./layers/requests_layer.zip"
 
   compatible_runtimes = ["python3.13"]
 }
@@ -172,20 +195,41 @@ resource "aws_lambda_layer_version" "requests_layer" {
 resource "aws_lambda_layer_version" "nltk_layer" {
 
   layer_name = "nltk_layer"
-  filename   = "./layers/nltk_layer_payload.zip"
+  filename   = "./layers/nltk_layer.zip"
 
   compatible_runtimes = ["python3.13"]
 }
 
-resource "aws_lambda_layer_version" "pyarrow_layer" {
+resource "aws_lambda_layer_version" "pandas_layer" {
 
-  layer_name = "pyarrow_layer"
-  filename   = "./layers/pyarrow_layer_payload.zip"
+  layer_name = "pandas_layer"
+  filename   = "./layers/pandas_layer.zip"
 
   compatible_runtimes = ["python3.13"]
 }
 
 # News Ingestion EventBridge Scheduler
+resource "aws_iam_role" "scheduler_role" {
+  name = "eventbridge-scheduler-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler_lambda_attachment" {
+  role       = aws_iam_role.scheduler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+}
 
 resource "aws_scheduler_schedule" "trigger_news_ingest" {
     name       = "trigger-news-ingest"
@@ -201,6 +245,6 @@ resource "aws_scheduler_schedule" "trigger_news_ingest" {
     
     target {
         arn = aws_lambda_function.ingest_news.arn
-        role_arn = aws_iam_role.lambda_execution_role.arn
+        role_arn = aws_iam_role.scheduler_role.arn
     }
 }
